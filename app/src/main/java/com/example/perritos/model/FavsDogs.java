bf0a5e0fa0108b836c31d058196bc1d59804fd13
Marcelo Vergara <marcelovergara@vigatec.cl>
2020-07-31 10:14:36 -0400

    Add Pojo db

---
+++ app/src/main/java/com/example/perritos/model/FavsDogs.java
@@ -0,0 +1,40 @@
+package com.example.perritos.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "favs_dogs")
+public class FavsDogs {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private int id;
+
+    private String breedName;
+    private String dogUrl;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBreedName() {
+        return breedName;
+    }
+
+    public void setBreedName(String breedName) {
+        this.breedName = breedName;
+    }
+
+    public String getDogUrl() {
+        return dogUrl;
+    }
+
+    public void setDogUrl(String dogUrl) {
+        this.dogUrl = dogUrl;
+    }
+}
